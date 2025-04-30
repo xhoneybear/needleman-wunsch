72b2f35b2d1daf8f93104c5dd4e3e9f1db097d62
Camille Lisek <camille@lisek.dev>
2025-04-30 13:40:43 +0200

    Add track animation
    When user inputs 0 for track choice, the tracks
    will be shown sequentially once every second

--- src/main/scala/dev/lisek/needleman_wunsch/plot/Plot.scala
+++ src/main/scala/dev/lisek/needleman_wunsch/plot/Plot.scala
@@ -12,18 +12,27 @@ import dev.lisek.needleman_wunsch.util.StringUtils.resultString
 
 object Plot:
     def plot(seq1: (String, String), seq2: (String, String), density: Array[Array[Double]], tracks: Array[(String, String)]): Unit =
+
         val data = DataMatrix(
             rows = density.flatten,
             numCols = density(0).length,
             numRows = density.length
         )
-
         val cellSize = 1.0/max(density.length, density(0).length)
         val width = (32*seq2(1).length*cellSize).fts
         val height = (32*seq1(1).length*cellSize).fts
         val xTicks = Seq((.0, "")) :++ (for (i <- 1 to seq2(1).length) yield (i.toDouble, seq2(1)(i-1).toString))
         val yTicks = Seq((.0, "")) :++ (for (i <- 1 to seq1(1).length) yield (i.toDouble, seq1(1)(i-1).toString))
-        var par2 = par
+
+        val heatmapParams = par
+            .xWidth(width)
+            .yHeight(height)
+            .xNumTicks(0)
+            .yNumTicks(0)
+            .xNoTickLabel(true)
+            .yNoTickLabel(true)
+
+        var trackParams = par
             .xWidth(width)
             .yHeight(height)
             .xlim(Some(-0.5, density(0).length - 0.5))
@@ -31,41 +40,35 @@ object Plot:
             .xgrid(false)
             .ygrid(false)
         if (seq2(0) == seq2(1))
-            par2 = par2
+            trackParams = trackParams
                 .xnames(xTicks)
                 .xLabFontSize((32*cellSize).fts)
         else
-            par2 = par2
+            trackParams = trackParams
                 .xlab(seq2(0))
                 .xLabFontSize(.75.fts)
                 
         if (seq1(0) == seq1(1))
-            par2 = par2
+            trackParams = trackParams
                 .ynames(yTicks)
                 .yLabFontSize((32*cellSize).fts)
         else
-            par2 = par2
+            trackParams = trackParams
                 .ylab(seq1(0))
                 .yLabFontSize(.75.fts)
-        
-        val plot1 = rasterplot(data)(par
-            .xWidth(width)
-            .yHeight(height)
-            .xNumTicks(0)
-            .yNumTicks(0)
-            .xNoTickLabel(true)
-            .yNoTickLabel(true)
-        )
 
-        def plot2(i: Int) = xyplot(tracePath(tracks(i)(0), tracks(i)(1)) -> line(
+        val heatmap = rasterplot(data)(heatmapParams)
+        def track(i: Int) = xyplot(tracePath(tracks(i)(0), tracks(i)(1)) -> line(
             color = Color(32, 48, 64),
             stroke = StrokeConf((8*cellSize).fts)
-        ))(par2)
+        ))(trackParams)
 
-        val plot = group(plot1, xyplot()(par2), ZDepth)
+        val plot = group(heatmap, xyplot()(trackParams), ZDepth)
 
         val (frame, update) = show(plot)
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE)
+
+        def updatePlot(i: Int) = update(group(heatmap, track(i), ZDepth))
 
         println(s"${tracks.size} best tracks found")
         println("Enter track number to display it:")
@@ -73,9 +76,13 @@ object Plot:
         while frame.isVisible do
             try
                 val i = readInt - 1
-                update(group(plot1, plot2(i), ZDepth))
-                print(resultString(tracks(i)(0), tracks(i)(1)))
-                // print("\u033c")
+                if i == -1 then
+                    Animation.start(tracks, updatePlot)
+                else if Animation.isAlive then
+                    Animation.stop
+                else
+                    update(group(heatmap, track(i), ZDepth))
+                    println(resultString(tracks(i)(0), tracks(i)(1)))
             catch
-                case _: NumberFormatException =>
-                case _: ArrayIndexOutOfBoundsException =>
+                case _: NumberFormatException => println("Please enter a number")
+                case _: ArrayIndexOutOfBoundsException => println("No such track")
